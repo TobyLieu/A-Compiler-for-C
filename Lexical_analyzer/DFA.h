840c80fffcb55471269adf01d5b3b5d9d48d3882
liutuo <616669577@qq.com>
2022-03-10 00:18:00 +0800

    add transition table and code table

--- Lexical_analyzer/DFA.h
+++ Lexical_analyzer/DFA.h
@@ -4,4 +4,4 @@
 #include <vector>
 using namespace std;
 
-int main() {}
+void initialize_DFA();
---
+++ Lexical_analyzer/code_table.h
@@ -0,0 +1,9 @@
+#include <unordered_map>
+using namespace std;
+
+extern unordered_map<string, int> iT;
+extern unordered_map<string, int> cT;
+extern unordered_map<string, int> sT;
+extern unordered_map<string, int> CT;
+extern unordered_map<string, int> KT;
+extern unordered_map<string, int> PT;
